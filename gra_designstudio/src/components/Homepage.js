e7b04805fe3238f227b57a8a4071523fe99343a1
ze-antunes <jprantunes2000@gmail.com>
2022-11-21 10:43:52 +0000

    add config.scss file

--- gra_designstudio/src/components/Homepage.js
+++ gra_designstudio/src/components/Homepage.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
 const Homepage = () => {
-    console.log("Estou dentro da Homepage")
+  console.log("Estou dentro da Homepage")
 
   return (
     <div>
-      homepage
+      <h1>Homepage</h1>
+      <h2>Homepage</h2>
+      <h3>Homepage</h3>
+      <h4>Homepage</h4>
+      <h5>Homepage</h5>
+      <h6>Homepage</h6>
+      <p>Homepage</p>
     </div>
   )
 }
