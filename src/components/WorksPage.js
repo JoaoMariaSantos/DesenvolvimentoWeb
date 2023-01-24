54d04376af548ca0ab452c30e4737aca9ad86288
ze-antunes <jprantunes2000@gmail.com>
2023-01-24 22:24:26 +0000

    Filtros semi feitos

--- src/components/WorksPage.js
+++ src/components/WorksPage.js
@@ -19,8 +19,10 @@ const WorksPage = () => {
                     if (data[i].id === projectIds[i] || data[i].slug === itemList[i]) {
                     } else {
                         projectIds.push(data[i].id)
-                        let str = data[i].slug.split('-').join(' ');
-                        itemList.push([str, data[i]])
+                        let projectName = data[i].slug.split('-').join(' ');
+                        let projectCategory = data[i].acf.categories
+                        let projectAuthor = data[i].acf.author[0]
+                        itemList.push([projectName, data[i], projectCategory, projectAuthor])
                     }
                 }
                 setItemList(itemList)
@@ -41,13 +43,48 @@ const WorksPage = () => {
         var updatedList = [...itemList];
         // Include all elements which includes the search query
         updatedList = updatedList.filter((item) => {
-            return item[0].toLowerCase().indexOf(query.toLowerCase()) !== -1;
+            let filter = item[0].toLowerCase().indexOf(query.toLowerCase()) !== -1;
+            return filter;
         });
         // Trigger render with updated values
         setFilteredList(updatedList);
         // console.log(filteredList)
     };
 
+    const onChangeCategory = (event) => {
+        const query = event.target.value;
+        // Create copy of item list
+        var updatedList = [...itemList];
+        // Include all elements which includes the search query
+        updatedList = updatedList.filter((item) => {
+            if(event.target.value !== "All") {
+                let filter = item[2][0].toLowerCase().indexOf(query.toLowerCase()) !== -1;
+                return filter
+            } else {
+                return updatedList = [...itemList];
+            }
+        });
+        // Trigger render with updated values
+        setFilteredList(updatedList);
+        // console.log(filteredList)
+    }
+
+    const onChangeAuthor = (event) => {
+        const query = event.target.value;
+        // Create copy of item list
+        var updatedList = [...itemList];
+        // Include all elements which includes the search query
+        updatedList = updatedList.filter((item) => {
+            if(event.target.value !== "All") {
+                return item[3].toLowerCase().indexOf(query.toLowerCase()) !== -1;
+            } else {
+                return updatedList = [...itemList];
+            }
+        });
+        // Trigger render with updated values
+        setFilteredList(updatedList);
+        // console.log(filteredList)
+    }
 
     return (
         <div className='workspage'>
@@ -57,24 +94,25 @@ const WorksPage = () => {
                     <ul className='projectfilter__unordered-list'>
                         <li className='projectfilter__list-item'>
                             <p className='projectfilter__lable'>Categories</p>
-                            <ul className='projectfilter__list-options'>
-                                <label htmlFor="all"><li><input type="radio" name="category" value="all" id="all" /> All</li></label>
-                                <label htmlFor="3d"><li><input type="radio" name="category" value="3d" id="3d" /> 3D</li></label>
-                                <label htmlFor="graphic_design"><li><input type="radio" name="category" value="graphic_design" id="graphic_design" /> Graphic Design</li></label>
-                                <label htmlFor="motion_design"><li><input type="radio" name="category" value="motion_design" id="motion_design" /> Motion Design</li></label>
-                                <label htmlFor="new_media"><li><input type="radio" name="category" value="new_media" id="new_media" /> New Media</li></label>
-                                <label htmlFor="photography"><li><input type="radio" name="category" value="photography" id="photography" /> Photography</li></label>
-                                <label htmlFor="video"><li><input type="radio" name="category" value="video" id="video" /> Video</li></label>
-                                <label htmlFor="web_design"><li><input type="radio" name="category" value="web_design" id="web_design" /> Web Design</li></label>
+                            <ul className='projectfilter__list-options' onChange={onChangeCategory}>
+                                <label htmlFor="all"><li><input type="radio" name="category" value="All" id="all" /> All</li></label>
+                                <label htmlFor="3d"><li><input type="radio" name="category" value="3D" id="3d" /> 3D</li></label>
+                                <label htmlFor="game_design"><li><input type="radio" name="category" value="Game Design" id="game_design" /> Game Design</li></label>
+                                <label htmlFor="graphic_design"><li><input type="radio" name="category" value="Graphic Design" id="graphic_design" /> Graphic Design</li></label>
+                                <label htmlFor="motion_design"><li><input type="radio" name="category" value="Motion Design" id="motion_design" /> Motion Design</li></label>
+                                <label htmlFor="new_media"><li><input type="radio" name="category" value="New Media" id="new_media" /> New Media</li></label>
+                                <label htmlFor="photography"><li><input type="radio" name="category" value="Photography" id="photography" /> Photography</li></label>
+                                <label htmlFor="video"><li><input type="radio" name="category" value="Video" id="video" /> Video</li></label>
+                                <label htmlFor="web_design"><li><input type="radio" name="category" value="Web Design" id="web_design" /> Web Design</li></label>
                             </ul>
                         </li>
                         <li className='projectfilter__list-item'>
                             <p className='projectfilter__lable'>Designer</p>
-                            <ul className='projectfilter__list-options'>
-                                <label htmlFor="all2"><li><input type="radio" name="designer" value="all" id="all2" /> All</li></label>
-                                <label htmlFor="ines"><li><input type="radio" name="designer" value="ines" id="ines" /> Inês</li></label>
-                                <label htmlFor="joao"><li><input type="radio" name="designer" value="joao" id="joao" /> João</li></label>
-                                <label htmlFor="jose"><li><input type="radio" name="designer" value="jose" id="jose" /> José</li></label>
+                            <ul className='projectfilter__list-options' onChange={onChangeAuthor}>
+                                <label htmlFor="all2"><li><input type="radio" name="designer" value="All" id="all2" /> All</li></label>
+                                <label htmlFor="ines"><li><input type="radio" name="designer" value="Inês Pereira" id="ines" /> Inês</li></label>
+                                <label htmlFor="joao"><li><input type="radio" name="designer" value="João Santos" id="joao" /> João</li></label>
+                                <label htmlFor="jose"><li><input type="radio" name="designer" value="José Antunes" id="jose" /> José</li></label>
                             </ul>
                         </li>
                         <li>
@@ -91,7 +129,7 @@ const WorksPage = () => {
             </div>
             <div className="masonry">
                 {filteredList.map((item, index) => {
-                    // return console.log(projects[index])
+                    // console.log(item)
                     // return <p key={index}>{item[1].slug}</p>
                     return <div key={index} className="grid">
                         <img src={item[1].acf.image_0.url} alt="" />
